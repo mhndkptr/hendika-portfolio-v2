8c71dfae0cff106517ac0fdf45f1672fa2694137
mhndkptr <muhamadhendika77@gmail.com>
2023-07-31 00:36:42 +0700

    uploading all file

---
+++ script.js
@@ -0,0 +1,22 @@
+const toTopBtn = document.querySelector(".toTopPage");
+const scrollBtnDisplay = function () {
+  if (window.scrollY >= 50) {
+    toTopBtn.style.display = "inline-block";
+  } else if (window.scrollY == 0) {
+    toTopBtn.style.display = "none";
+  }
+};
+window.addEventListener("scroll", scrollBtnDisplay);
+
+const navbarMenu = document.querySelector('.navbarMenu');
+const navbarNav = document.querySelector('nav .navbarNav');
+const nav = document.querySelector('nav');
+navbarMenu.addEventListener('click', function() {
+    navbarNav.classList.toggle('active');
+});
+
+document.addEventListener('click', function(e) {
+    if( !navbarNav.contains(e.target) && !nav.contains(e.target) ) {
+        navbarNav.classList.remove('active');
+    }
+});
